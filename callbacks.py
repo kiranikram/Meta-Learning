cef50f37625c2385315f0c3e8545059abf42837d
kiranikram <45267484+kiranikram@users.noreply.github.com>
2020-05-31 10:32:57 +0100

    Add files via upload
    Callbacks for MAML implementation

---
+++ callbacks.py
@@ -0,0 +1,255 @@
+
+from tqdm import tqdm
+import numpy as np
+import torch
+from collections import OrderedDict, Iterable
+import warnings
+import os
+import csv
+import io
+
+
+class CallbackList(object):
+    def __init__(self, callbacks):
+        self.callbacks = [c for c in callbacks]
+
+    def set_params(self, params):
+        for callback in self.callbacks:
+            callback.set_params(params)
+
+    def set_model(self, model):
+        for callback in self.callbacks:
+            callback.set_model(model)
+
+    def on_epoch_begin(self, epoch, logs=None):
+        """Called at the start of an epoch.
+        # Arguments
+            epoch: integer, index of epoch.
+            logs: dictionary of logs.
+        """
+        logs = logs or {}
+        for callback in self.callbacks:
+            callback.on_epoch_begin(epoch, logs)
+
+    def on_epoch_end(self, epoch, logs=None):
+        """Called at the end of an epoch.
+        # Arguments
+            epoch: integer, index of epoch.
+            logs: dictionary of logs.
+        """
+        logs = logs or {}
+        for callback in self.callbacks:
+            callback.on_epoch_end(epoch, logs)
+
+    def on_batch_begin(self, batch, logs=None):
+        """Called right before processing a batch.
+        # Arguments
+            batch: integer, index of batch within the current epoch.
+            logs: dictionary of logs.
+        """
+        logs = logs or {}
+        for callback in self.callbacks:
+            callback.on_batch_begin(batch, logs)
+
+    def on_batch_end(self, batch, logs=None):
+        """Called at the end of a batch.
+        # Arguments
+            batch: integer, index of batch within the current epoch.
+            logs: dictionary of logs.
+        """
+        logs = logs or {}
+        for callback in self.callbacks:
+            callback.on_batch_end(batch, logs)
+
+    def on_train_begin(self, logs=None):
+        """Called at the beginning of training.
+        # Arguments
+            logs: dictionary of logs.
+        """
+        logs = logs or {}
+        for callback in self.callbacks:
+            callback.on_train_begin(logs)
+
+    def on_train_end(self, logs=None):
+        """Called at the end of training.
+        # Arguments
+            logs: dictionary of logs.
+        """
+        logs = logs or {}
+        for callback in self.callbacks:
+            callback.on_train_end(logs)
+
+
+class Callback(object):
+    def __init__(self):
+        self.model = None
+
+    def set_params(self, params):
+        self.params = params
+
+    def set_model(self, model):
+        self.model = model
+
+    def on_epoch_begin(self, epoch, logs=None):
+        pass
+
+    def on_epoch_end(self, epoch, logs=None):
+        pass
+
+    def on_batch_begin(self, batch, logs=None):
+        pass
+
+    def on_batch_end(self, batch, logs=None):
+        pass
+
+    def on_train_begin(self, logs=None):
+        pass
+
+    def on_train_end(self, logs=None):
+        pass
+
+
+class DefaultCallback(Callback):
+    """Records metrics over epochs by averaging over each batch.
+
+    NB The metrics are calculated with a moving model
+    """
+    def on_epoch_begin(self, batch, logs=None):
+        self.seen = 0
+        self.totals = {}
+        self.metrics = ['loss'] + self.params['metrics']
+
+    def on_batch_end(self, batch, logs=None):
+        logs = logs or {}
+        batch_size = logs.get('size', 1) or 1
+        self.seen += batch_size
+
+        for k, v in logs.items():
+            if k in self.totals:
+                self.totals[k] += v * batch_size
+            else:
+                self.totals[k] = v * batch_size
+
+    def on_epoch_end(self, epoch, logs=None):
+        if logs is not None:
+            for k in self.metrics:
+                if k in self.totals:
+                    # Make value available to next callbacks.
+                    logs[k] = self.totals[k] / self.seen
+
+
+class ProgressBarLogger(Callback):
+    """TQDM progress bar that displays the running average of loss and other metrics."""
+    def __init__(self):
+        super(ProgressBarLogger, self).__init__()
+
+    def on_train_begin(self, logs=None):
+        self.num_batches = self.params['num_batches']
+        self.verbose = self.params['verbose']
+        self.metrics = ['loss'] + self.params['metrics']
+
+    def on_epoch_begin(self, epoch, logs=None):
+        self.target = self.num_batches
+        self.pbar = tqdm(total=self.target, desc='Epoch {}'.format(epoch))
+        self.seen = 0
+
+    def on_batch_begin(self, batch, logs=None):
+        self.log_values = {}
+
+    def on_batch_end(self, batch, logs=None):
+        logs = logs or {}
+        self.seen += 1
+
+        for k in self.metrics:
+            if k in logs:
+                self.log_values[k] = logs[k]
+
+        # Skip progbar update for the last batch;
+        # will be handled by on_epoch_end.
+        if self.verbose and self.seen < self.target:
+            self.pbar.update(1)
+            self.pbar.set_postfix(self.log_values)
+
+    def on_epoch_end(self, epoch, logs=None):
+        # Update log values
+        self.log_values = {}
+        for k in self.metrics:
+            if k in logs:
+                self.log_values[k] = logs[k]
+
+        if self.verbose:
+            self.pbar.update(1)
+            self.pbar.set_postfix(self.log_values)
+
+        self.pbar.close()
+
+
+class CSVLogger(Callback):
+    """Callback that streams epoch results to a csv file.
+    Supports all values that can be represented as a string,
+    including 1D iterables such as np.ndarray.
+
+    # Arguments
+        filename: filename of the csv file, e.g. 'run/log.csv'.
+        separator: string used to separate elements in the csv file.
+        append: True: append if file exists (useful for continuing
+            training). False: overwrite existing file,
+    """
+
+    def __init__(self, filename, separator=',', append=False):
+        self.sep = separator
+        self.filename = filename
+        self.append = append
+        self.writer = None
+        self.keys = None
+        self.append_header = True
+        self.file_flags = ''
+        self._open_args = {'newline': '\n'}
+        super(CSVLogger, self).__init__()
+
+    def on_train_begin(self, logs=None):
+        if self.append:
+            if os.path.exists(self.filename):
+                with open(self.filename, 'r' + self.file_flags) as f:
+                    self.append_header = not bool(len(f.readline()))
+            mode = 'a'
+        else:
+            mode = 'w'
+
+        self.csv_file = io.open(self.filename,
+                                mode + self.file_flags,
+                                **self._open_args)
+
+    def on_epoch_end(self, epoch, logs=None):
+        logs = logs or {}
+
+        def handle_value(k):
+            is_zero_dim_ndarray = isinstance(k, np.ndarray) and k.ndim == 0
+            if isinstance(k, str):
+                return k
+            elif isinstance(k, Iterable) and not is_zero_dim_ndarray:
+                return '"[%s]"' % (', '.join(map(str, k)))
+            else:
+                return k
+
+        if self.keys is None:
+            self.keys = sorted(logs.keys())
+
+        if not self.writer:
+            class CustomDialect(csv.excel):
+                delimiter = self.sep
+            fieldnames = ['epoch'] + self.keys
+            self.writer = csv.DictWriter(self.csv_file,
+                                         fieldnames=fieldnames,
+                                         dialect=CustomDialect)
+            if self.append_header:
+                self.writer.writeheader()
+
+        row_dict = OrderedDict({'epoch': epoch})
+        row_dict.update((key, handle_value(logs[key])) for key in self.keys)
+        self.writer.writerow(row_dict)
+        self.csv_file.flush()
+
+    def on_train_end(self, logs=None):
+        self.csv_file.close()
+        self.writer = None
